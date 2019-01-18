700fd813b358b3638cb6efe885f3ed416b9517e0
SriYasaswini <kandarpa.sriyasaswini@gmail.com>
2019-01-18 10:42:23 +0530

    new start2

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,4 @@
 import React from "react";
-import Hello from "./incrementer/index";
-import Incrementer from "./incrementer/index";
-import Table from "./table/index";
-import View from "./view/index";
-import Form from "./form/index"
-
 import Home from "./restaurant";
 
 
