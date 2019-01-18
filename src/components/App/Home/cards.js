7eda3360649ef5216a35c9cea078f815fa5ae766
SriYasaswini <kandarpa.sriyasaswini@gmail.com>
2019-01-18 15:37:28 +0530

    final card

--- src/components/App/Home/cards.js
+++ src/components/App/Home/cards.js
@@ -7,17 +7,17 @@ import { Card, CardImg, CardText, CardBody,
 class Cards extends React.Component {
        render() {
         return(
-            <div>
+            
             <Card>
               <CardImg top width="100%" alt="Card image cap"/>
               <CardBody>
                 <CardTitle>Card title</CardTitle>
                 <CardSubtitle>Card subtitle</CardSubtitle>
                 <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                <Button>Button</Button>
+                <Button>Button</Button> 
               </CardBody>
             </Card>
-            </div>
+            
             )
     }
 }
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,28 @@
 import React from "react";
 import Header from "./home/header";
 import Cards from "./home/cards";
-
+import {CardDeck} from "reactstrap";
+import {CardColumns} from "reactstrap";
 
 class App extends React.Component {
        
     render() {
         return (
+            
+                <CardColumns>
                 <Cards/>
+                {/* <Cards/>
+                <Cards/>
+                <Cards/>
+                <Cards/>
+                <Cards/><Cards/>
+                <Cards/> */}
+                </CardColumns>
+                // <div>
+                // <Cards/>
+                // <Cards/>
+                // </div>
+
         );
     }
 } 
