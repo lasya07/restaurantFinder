b0e82717368805d2992ccd5a73451c12b40c44d0
SriYasaswini <kandarpa.sriyasaswini@gmail.com>
2019-01-18 10:39:01 +0530

    new start

--- src/components/App/restaurant/index.js
+++ src/components/App/restaurant/index.js
@@ -7,13 +7,13 @@ class Home extends React.Component{
             <form onSubmit = {this.onFormSubmit} >
                 <h1>WEB APP</h1>
                 <hr></hr>
-                <select name="cars">
-                    <option value="volvo">Volvo</option>
-                    <option value="saab">Saab</option>
-                    <option value="fiat">Fiat</option>
-                    <option value="audi">Audi</option>
+                <select name="Places">
+                    <option value="Hyderabad">Hyderabad</option>
+                    <option value="Pune">Pune</option>
+                    <option value="Mumbai">Mumbai</option>
                 </select>
-                <label>Alias:<input type = "text" onChange = {this.onAliasChange} /></label>
+                <input type="text" placeholder="Search.."/> 
+                <button type="submit">Submit</button>
                 <label>Team: <input type = "text" onChange = {this.onTeamChange} /></label>
                 <button>Submit</button>
             </form>
