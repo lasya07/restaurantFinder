ae1d2b4bd25e31b2f0816b6603fb85676bc12a41
lasya07 <lasya.sam@gmail.com>
2019-01-24 11:56:10 +0530

    added login files

---
+++ src/components/App/login/login.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import { Form, Button, FormGroup, FormControl, ControlLabel,Col,ButtonGroup } from "react-bootstrap";
+import "./Login.css";
+import { withRouter } from 'react-router-dom';
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+
+    this.routeChange = this.routeChange.bind(this);
+  }
+
+  routeChange(){
+    let path = `signup`;
+    this.props.history.push(path);
+    }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 5;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+     
+        <Form onSubmit={this.handleSubmit}>
+          <h1>Login Here!!</h1>
+          <Form horizontal>
+          <FormGroup controlId="email">
+            <Col componentClass={ControlLabel} sm={2}>
+              Email
+            </Col>
+            <Col sm={10}>
+              <FormControl type="email" placeholder="Email" 
+                           value = {this.state.email} 
+                           onChange = {this.handleChange}
+              />
+            </Col>
+          </FormGroup>
+          
+         
+          <FormGroup controlId="password">
+            <Col componentClass={ControlLabel} sm={2}>
+              Password
+            </Col>
+            <Col sm={10}>
+              <FormControl type="password" placeholder="Password" 
+                            value={this.state.password}
+                            onChange={this.handleChange}
+              />
+            </Col>
+          </FormGroup>
+
+         
+          <FormGroup>
+            <ButtonGroup>
+            {/* <Col smOffset={2} sm={10}>
+              <Button  type="submit">Sign in</Button>
+            </Col> */}
+            <Col smOffset={2} sm={100}>
+              <Button onClick={this.routeChange} type="submit">Sign in</Button>
+            </Col>
+            </ButtonGroup>
+          </FormGroup>
+        </Form>
+
+        </Form>
+     
+    );
+  }
+}
---
+++ src/components/App/login/signup.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import { Form, Button, FormGroup, FormControl, ControlLabel,Col,PageHeader } from "react-bootstrap";
+import "./Login.css";
+
+export default class signup extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      confirmPassword: ""
+    };
+  }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 5 && this.state.password == this.state.confirmPassword;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+            <Form onSubmit={this.handleSubmit}>
+            
+            <Form horizontal>
+                <PageHeader>
+                    Register Here!! 
+                </PageHeader>
+
+                <FormGroup controlId="email">
+                    <Col componentClass={ControlLabel} sm={2}> Email </Col>
+                    <Col sm={10}>
+                        <FormControl type="email" placeholder="Email" 
+                                    value = {this.state.email} 
+                                    onChange = {this.handleChange}
+                        />
+                    </Col>
+                </FormGroup>
+                
+                <FormGroup controlId="password">
+                    <Col componentClass={ControlLabel} sm={2}>Password</Col>
+                    <Col sm={10}>
+                    <FormControl type="password" placeholder="Password" 
+                                    value={this.state.password}
+                                    onChange={this.handleChange}
+                    />
+                    </Col>
+                </FormGroup>
+
+                <FormGroup controlId="confirmPassword">
+                    <Col componentClass={ControlLabel} sm={2}>Password</Col>
+                    <Col sm={10}>
+                    <FormControl type="password" placeholder=" Confirm Password" 
+                                    value={this.state.confirmPassword}
+                                    onChange={this.handleChange}
+                    />
+                    </Col>
+                </FormGroup>  
+            
+                <FormGroup>
+                    
+                    <Col smOffset={2} sm={10}>
+                    <Button href="#" type="submit" disabled={!this.validateForm()} >Register</Button>
+                    </Col>
+                                
+                </FormGroup>
+            </Form>
+           </Form>           
+    );
+  }
+}
