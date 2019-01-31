41f43230debd585977b0846a4d8358a3c6e192ab
SriYasaswini <kandarpa.sriyasaswini@gmail.com>
2019-01-31 09:49:47 +0530

    photos

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Home from "./Home/index"
+import Details from "./restoDetails";
 
 class App extends React.Component {
        
     render() {
         return (
             
-            <Home/>
+           // <Home/>
+           <Details/>
 
         );
     }
---
+++ src/components/App/restoDetails/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Photo from "./photos";
+
+class Details extends React.Component {
+       
+    render() {
+        return (
+            
+            <Photo/>
+
+        );
+    }
+} 
+
+export default Details;
